e15299dc64931b6063ce657ce479b9d11cdb0eb4
RafaelRuizSilva <rafaruizabc@gmail.com>
2026-01-19 22:30:17 -0300

    fix: date

--- app/core/models/destinatario.py
+++ app/core/models/destinatario.py
@@ -25,7 +25,7 @@ class Destinatario:
                 ).date()
             except ValueError:
                 raise ValueError(
-                    f"data_conclusao deve ser date ou string no formato DD/MM/YYYY, a atual é {self.data_conclusao}"
+                    f"data_conclusao deve ser date ou string no formato DD/MM/YYYY, a atual é {data_conclusao}"
                 )
 
         self.data_atual = datetime.now().date()
